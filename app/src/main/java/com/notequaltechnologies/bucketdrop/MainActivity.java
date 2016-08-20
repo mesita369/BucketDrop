9b5399a2e1ceeb42bcdd7921da08b4e20cfbd07c
mesita369 <mesita369@gmail.com>
2016-08-21 00:38:02 +0530

    Initial commit

---
+++ app/src/main/java/com/notequaltechnologies/bucketdrop/MainActivity.java
@@ -0,0 +1,13 @@
+package com.notequaltechnologies.bucketdrop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
